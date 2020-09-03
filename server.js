42168e0a61fec9d3d9edc8581cf73eb52dceb88b
manuelsanchez2 <manusansan22@gmail.com>
2020-09-03 13:17:32 +0200

    Serve client and storybook

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 // console.log(process.env);
 
 // Serve any static files
-app.use(express.static("client/build"));
+app.use(express.static(path.join(__dirname, "client/build")));
+app.use(
+  "/storybook",
+  express.static(path.join(__dirname, "client/storybook-static"))
+);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
