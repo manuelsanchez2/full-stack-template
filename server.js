354732a1dc4e8c225568c356bd3f1b87fea900d0
manuelsanchez2 <manusansan22@gmail.com>
2020-09-03 10:29:58 +0200

    Read PORT from environment variable

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+// console.log(process.env);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
